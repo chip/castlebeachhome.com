47db496c10531934c23fbea9f00e3dfdec20c9e1
Chip Castle <chip@chipcastle.com>
2022-03-01 16:29:06 -0600

    Update Gulf Place images to use lazyload.js, resized to 495x660, converted to jpg

--- js/main.js
+++ js/main.js
@@ -76,6 +76,19 @@
             }
         }
     });
-    
+
+    // Gulf Place Residences carousel
+    $(".owl-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
 })(jQuery);
 
