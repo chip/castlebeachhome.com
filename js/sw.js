148f7c2e0f4f8cce5b63efb993c501a2b1dee5aa
Chip Castle <chip@chipcastle.com>
2022-03-07 11:22:02 -0600

    Add imagekit service worker

---
+++ js/sw.js
@@ -0,0 +1,2 @@
+// copy paste the below line inside your service worker code loaded with the root scope
+importScripts("https://runtime.imagekit.io/0gr1w07bzr6iu/v1/js/network-based-adaption.js?v=" + new Date().getTime())
